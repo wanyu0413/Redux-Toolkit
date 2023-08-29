34cb4be4331c46e2e7dda67ac9c8f1f40d728596
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-29 17:57:32 +0900

    import actions to index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
 
 console.log('Initial state ', store.getState())
 
@@ -11,5 +12,9 @@ store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
+
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(2))
 
 unsubscribe()
