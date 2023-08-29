c0533dd14a79d8d2339ee5877554d5f7e626e71c
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-29 18:41:39 +0900

    reduxLogger

--- app/store.js
+++ app/store.js
@@ -1,6 +1,10 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
+const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
+const { getDefaultMiddleware } = require('@reduxjs/toolkit')
+
+const logger = reduxLogger.createLogger
 
 // we don't need combineReducers to combine 2 reducers, because configureStore will handle it under the hood
 const store = configureStore({
@@ -8,6 +12,8 @@ const store = configureStore({
         cake: cakeReducer,
         icecream: icecreamReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
+    // we append the logger middleware to the list of default middleware
 })
 
 module.exports = store
