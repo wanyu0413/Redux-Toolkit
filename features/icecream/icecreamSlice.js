8312678b33c7e2525e15ea6c245a52bbcaf2dd4c
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-29 19:44:35 +0900

    extraReducer with addCase

--- features/icecream/icecreamSlice.js
+++ features/icecream/icecreamSlice.js
@@ -1,3 +1,4 @@
+const { cakeActions } = require('../cake/cakeSlice')
 const createSlice = require('@reduxjs/toolkit').createSlice
 
 const initialState = {
@@ -15,10 +16,15 @@ const icecreamSlice = createSlice({
             state.numOfIcecreams += action.payload
         },
     },
-    extraReducers: {
-        ['cake/ordered']: (state) => {
+    // extraReducers: {
+    //     ['cake/ordered']: (state) => {
+    //         state.numOfIcecreams--
+    //     }
+    // }
+    extraReducers: (builder) => {
+        builder.addCase(cakeActions.ordered, state => {
             state.numOfIcecreams--
-        }
+        })
     }
 })
 
