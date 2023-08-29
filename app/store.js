f6be4a62503ef4e55bd18adaccdd268fd8c9aac0
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-29 18:02:09 +0900

    add comments

--- app/store.js
+++ app/store.js
@@ -2,6 +2,7 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
 
+// we don't need combineReducers to combine 2 reducers, because configureStore will handle it under the hood
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
