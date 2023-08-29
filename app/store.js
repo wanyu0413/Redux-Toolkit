fa54f20d9a0fac65c7038680febdab2e84d4ef1a
Wanyu Jiang <wanyu19930930@gmail.com>
2023-08-29 20:32:34 +0900

    add userRuducer to store.js

--- app/store.js
+++ app/store.js
@@ -2,6 +2,7 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const reduxLogger = require('redux-logger')
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/icecream/icecreamSlice')
+const userReducer = require('../features/user/userSlice')
 const { getDefaultMiddleware } = require('@reduxjs/toolkit')
 
 const logger = reduxLogger.createLogger()
@@ -11,6 +12,7 @@ const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
+        user: userReducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
     // we append the logger middleware to the list of default middleware
